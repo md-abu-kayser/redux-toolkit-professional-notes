c93cb0e22fc197992df8c81908bb1e2a163625fb
md-abu-kayser <abu.kayser.official@gmail.com>
2026-08-02 20:32:51 +0600

    feat(hooks): add guide on typed hooks for redux store

--- 13-code-snippets/typed-hooks.ts
+++ 13-code-snippets/typed-hooks.ts
@@ -1,2 +1,4 @@
-// Typed hooks
-// TODO: add implementation and example usage
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import type { RootState, AppDispatch } from "./store";
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
