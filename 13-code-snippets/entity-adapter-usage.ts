7bbf1a365fe6395953fa8395ad919405e552cebb
md-abu-kayser <abu.kayser.official@gmail.com>
2026-08-02 20:32:03 +0600

    feat(entity-adapter): add guide on createEntityAdapter with sorted users selectors

--- 13-code-snippets/entity-adapter-usage.ts
+++ 13-code-snippets/entity-adapter-usage.ts
@@ -1,2 +1,7 @@
-// Entity adapter usage
-// TODO: add implementation and example usage
+import { createEntityAdapter } from "@reduxjs/toolkit";
+import { User } from "./types";
+const usersAdapter = createEntityAdapter<User>({
+  sortComparer: (a, b) => a.name.localeCompare(b.name),
+});
+export const { selectAll: selectAllUsers, selectById: selectUserById } =
+  usersAdapter.getSelectors<RootState>((state) => state.users);
